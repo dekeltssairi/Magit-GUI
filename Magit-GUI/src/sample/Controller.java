a44c67ffb850ce7563a1f923de0e9e109f4d34e9
Noy Wolfson <noy@wolfson.co.il>
2019-08-20 13:56:10 +0300

    UserName window is done

--- Magit-GUI/src/sample/Controller.java
+++ Magit-GUI/src/sample/Controller.java
@@ -1,16 +1,16 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 import logic.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -23,28 +23,59 @@ public class Controller implements Initializable {
         m_MyAmazingGitEngine = new MyAmazingGitEngine();
         m_UserName.setText("Administrator");
     }
-    
-    public void SetUserName(ActionEvent i_Event){
+
+    @FXML private TextField m_PathToRepository;
+
+    public void  initializeRepository(ActionEvent i_Event) {
+        m_MyAmazingGitEngine.LoadExistRepository(m_PathToRepository.getText());
+    }
+
+    @FXML
+    public void newWindow(ActionEvent i_Event) {
+        try {
+            //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("InitializeRepository.fxml"));
+           // fxmlLoader.setController(this);
+            Parent root =  FXMLLoader.load(getClass().getResource("InitializeRepository.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void SetUserName(ActionEvent i_Event) throws IOException {
         Label secondLabel = new Label("Change UserName");
         TextField textField = new TextField();
         Button button = new Button("Change");
+
+        VBox userNameWindow = new VBox();
+        HBox userNameField = new HBox();
+        userNameField.getChildren().addAll(secondLabel, textField);
+        userNameField.setSpacing(10);
+        userNameWindow.getChildren().addAll(userNameField, button);
+        userNameWindow.setAlignment(Pos.CENTER);
+        userNameField.setAlignment(Pos.CENTER);
+
+        userNameWindow.setSpacing(10);
+        Scene secondScene = new Scene(userNameWindow, 230, 100);
+        Stage newWindow = new Stage();
+        newWindow.setHeight(100);
+        newWindow.setWidth(300);
+        newWindow.setResizable(false);
+        newWindow.setTitle("Set UserName");
+        newWindow.setScene(secondScene);
+        newWindow.initModality(Modality.APPLICATION_MODAL);
+        newWindow.show();
+
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 m_MyAmazingGitEngine.SetUsername(textField.getText());
                 m_UserName.setText(textField.getText());
+                newWindow.close();
             }
         });
-        HBox secondaryLayout = new HBox();
-        secondaryLayout.getChildren().addAll(secondLabel, textField, button);
-
-        Scene secondScene = new Scene(secondaryLayout, 230, 100);
-
-        // New window (Stage)
-        Stage newWindow = new Stage();
-        newWindow.setTitle("Set UserName");
-        newWindow.setScene(secondScene);
-        newWindow.show();
     }
 }
 
--- Magit-GUI/src/sample/Main.java
+++ Magit-GUI/src/sample/Main.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 
@@ -16,11 +16,10 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 748, 518));
         primaryStage.show();
 
+
     }
 
     public static void main(String[] args) {
-        System.out.println("This is After Adding Noy");
-        System.out.println("fdd");
         launch(args);
     }
 }
