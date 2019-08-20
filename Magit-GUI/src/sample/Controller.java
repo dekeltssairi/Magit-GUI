06a31bb47768c3a3b5fa896a6ce5be68c828ba2f
Noy Wolfson <noy@wolfson.co.il>
2019-08-20 20:08:07 +0300

    split initializerepository.fxml and sample.fxml Exception in fxmlLoader.load();

--- Magit-GUI/src/sample/Controller.java
+++ Magit-GUI/src/sample/Controller.java
@@ -3,12 +3,10 @@ package sample;
 import javafx.event.*;
 import javafx.fxml.*;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+import javafx.stage.*;
 import logic.*;
 import java.io.IOException;
 import java.net.URL;
@@ -18,6 +16,7 @@ public class Controller implements Initializable {
 
     private MyAmazingGitEngine m_MyAmazingGitEngine;
     @FXML private Label m_UserName;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         m_MyAmazingGitEngine = new MyAmazingGitEngine();
@@ -31,11 +30,19 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    public void newWindow(ActionEvent i_Event) {
+    public void InitializeRepositoryWindow(ActionEvent i_Event) {
         try {
             //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("InitializeRepository.fxml"));
            // fxmlLoader.setController(this);
-            Parent root =  FXMLLoader.load(getClass().getResource("InitializeRepository.fxml"));
+            //Parent root =  FXMLLoader.load(getClass().getResource("InitializeRepository.fxml"));
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+//            URL url = getClass().getResource("../InitializeRepository/InitializeRepository.fxml");
+//            fxmlLoader.setLocation(url);
+//            Parent root =  fxmlLoader.load(url.openStream());
+
+            Parent root = fxmlLoader.load(getClass().getResource("C:\\Users\\noywo\\java\\Magit-GUI\\Magit-GUI\\src\\sample\\InitializeRepository\\InitializeRepository.fxml"));
+
             Stage stage = new Stage();
             stage.setScene(new Scene(root));
             stage.show();
@@ -78,4 +85,3 @@ public class Controller implements Initializable {
         });
     }
 }
-
---
+++ Magit-GUI/src/sample/InitializeRepository/InitializeRepositoryController.java
@@ -0,0 +1,13 @@
+package sample.InitializeRepository;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class InitializeRepositoryController {
+    @FXML private TextField m_PathToRepository;
+    @FXML private TextField m_RepositoryName;
+    @FXML private Button m_LoadBtn;
+
+
+
+}
